0ffa932a9eb733fb1f035377c3f18ccb905ebd96
John Ehrlinger <john.ehrlinger@gmail.com>
2018-12-07 10:18:05 -0500

    Create config.py

---
+++ scripts/config.py
@@ -0,0 +1,23 @@
+def inplace_change(filename, old_string, new_string):
+    # Safely read the input filename using 'with'
+    with open(filename) as f:
+        s = f.read()
+        if old_string not in s:
+            print '"{old_string}" not found in {filename}.'.format(**locals())
+            return
+
+    # Safely write the changed content, if found in the file
+    with open(filename, 'w') as f:
+        print 'Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals())
+        s = s.replace(old_string, new_string)
+        f.write(s)
+
+
+# Module sys has to be imported:
+import sys                
+
+# Iteration over all arguments:
+for eachArg in sys.argv:   
+        print(eachArg)
+
+        
