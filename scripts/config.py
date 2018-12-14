e1ca20440dd84bf37c17af8e3edb7ec582960efd
John Ehrlinger <john.ehrlinger@gmail.com>
2018-12-14 14:26:12 -0500

    config.py
    fill in clusterID and username in databricks job json files.

--- scripts/config.py
+++ scripts/config.py
@@ -1,54 +1,44 @@
 import argparse
-
-
-def inplace_change(filename, old_string, new_string):
-    # Safely read the input filename using 'with'
-    with open(filename) as f:
-        s = f.read()
-        if old_string not in s:
-            print('"{old_string}" not found in {filename}.'.format(**locals()))
-            return
-
-    # Safely write the changed content, if found in the file
-    with open(filename, "w") as f:
-        print(
-            'Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals())
-        )
-        s = s.replace(old_string, new_string)
-        f.write(s)
-
-
-# Module sys has to be imported:
+import os
+import glob
 import sys
-
-print("Number of arguments:", len(sys.argv), "arguments.")
-print("Argument List:", str(sys.argv))
-
-#
 
 parser = argparse.ArgumentParser(
     description="searches the jobs folder for Databricks Jobs json files,\n"
-    + "and configures the scripts to connect to the clusterID and username provided.\n"
-    + ""
+    + "and configures the scripts to connect to the clusterID and username provided."
 )
 parser.add_argument(
-    "clusterID",
-    metavar="--clusterID",
+    "-c",
+    "--clusterID",
     type=str,
-    nargs="+",
     help="The cluster ID is found using the databricks CLI command:\n"
-    + "\ndatabricks cluster list\n",
+    + "\ndatabricks clusters list\n",
 )
 parser.add_argument(
-    "username",
-    metavar="--username",
+    "-u",
+    "--username",
     type=str,
-    nargs="+",
     help="Found from the Databricks Workspace and should look like\n"
     + "your active directory email address.",
 )
+parser.add_argument("infile", type=str)
 
 args = parser.parse_args()
 
 print(args.clusterID)
 print(args.username)
+print(args.infile)
+
+
+for filename in glob.glob(os.path.join(args.infile, "*.json")):
+    with open(filename, "r") as f:
+        s = f.read()
+        if "<clusterid>" in s:
+            s = s.replace("<clusterid>", args.clusterID)
+        if "<uname@example.com>" in s:
+            s = s.replace("<uname@example.com>", args.username)
+
+    fname = filename.replace("tmpl", "json")
+    with open(fname, "w") as fs:
+        fs.write(s)
+
